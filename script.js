9fd0fb90cd3a3a5e21ccc7a5371fcaa33362b336
Gabrielrabelo05 <sergio.gpr05@gmail.com>
2023-10-29 17:36:40 -0300

    add-cart

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 // Opem-menu
 let menu = document.querySelector("#menu-icon")
 let navmenu = document.querySelector(".navmenu")
-let car = document.querySelector(".bx-cart")
 
 menu.onclick = () => {
     menu.classList.toggle("bx-x")
     navmenu.classList.toggle("open")
 }
-let test =  document.querySelector(".add-car")
-
-function add(click) {
+// alert-addcar
+let addcar =  document.querySelector(".add-car")
+function add(addcar) {
     alert("Produto adicionado ao carrinho")
+}
+// abrir e fechar cart
+let cart = document.querySelector("#cart")
+function openCart() {
+    if(cart.classList.contains("hidden")) {
+        cart.classList.remove("hidden")
+    } else {
+        cart.classList.add("hidden")
+    }
 }
