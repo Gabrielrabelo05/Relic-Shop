4e1807d6e92d0c4bf73c0b0d51a0dfdce3516eb3
Gabrielrabelo05 <sergio.gpr05@gmail.com>
2023-11-01 01:43:56 -0300

    add\js

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
-// Opem-menu
-let menu = document.querySelector("#menu-icon")
-let navmenu = document.querySelector(".navmenu")
+//  Abrir o menu
+let menu = document.querySelector("#menu-icon");
+let navmenu = document.querySelector(".navmenu");
 
 menu.onclick = () => {
-    menu.classList.toggle("bx-x")
-    navmenu.classList.toggle("open")
-}
-// alert-addcar
-let addcar =  document.querySelector(".add-car")
-function add(addcar) {
-    alert("Produto adicionado ao carrinho")
-}
-// abrir e fechar cart
-let cart = document.querySelector("#cart")
+    menu.classList.toggle("bx-x");
+    navmenu.classList.toggle("open");
+};
+
+// Abrir e fechar o carrinho
+let cart = document.querySelector("#cart");
+
 function openCart() {
-    if(cart.classList.contains("hidden")) {
-        cart.classList.remove("hidden")
+    if (cart.classList.contains("hidden")) {
+        cart.classList.remove("hidden");
     } else {
-        cart.classList.add("hidden")
+        cart.classList.add("hidden");
     }
+}
+// Verificar o estado de carregamento do documento.
+if (document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", ready);
+} else {
+    ready();
+}
+
+// function ready() {
+//     var removeBtnCart = document.getElementsByClassName("box-cart");
+//     for (var i = 0; i < removeBtnCart.length; i++) {
+//         var btn = removeBtnCart[i];
+//         btn.addEventListener("click", removeCartItem);
+//     }
+// }
+
+// function removeCartItem(event) {
+//     Lógica para remover um item do carrinho aqui
+//     var clickedBtn = event.target
+//     clickedBtn.parentElement.remove()
+//     alert("Produto retirado do carrinho")
+// }
+
+
+function ready() {
+    var removeBtnCart = document.getElementsByClassName("box-cart");
+    for (var i = 0; i < removeBtnCart.length; i++) {
+        var btn = removeBtnCart[i];
+        btn.addEventListener("click", removeCartItem);
+    }
+}
+function removeCartItem(event) {
+    // Navegar até o elemento pai do botão (o item do carrinho) e removê-lo
+    var clickedBtn = event.target;
+    var cartItem = clickedBtn.parentElement;
+    cartItem.parentElement.removeChild(cartItem);
+    // alert("Produto retirado do carrinho")
 }
