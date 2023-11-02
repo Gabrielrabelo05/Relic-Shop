ee620e4a2a9515ae32c7d96f09884402ea2a2752
Gabrielrabelo05 <sergio.gpr05@gmail.com>
2023-11-02 01:17:41 -0300

    add-cart

--- script.js
+++ script.js
@@ -31,10 +31,59 @@ function ready() {
         btn.addEventListener("click", removeCartItem);
     }
 }
+
 function removeCartItem(event) {
     // Navegar até o elemento pai do botão (o item do carrinho) e removê-lo
     var clickedBtn = event.target;
     var cartItem = clickedBtn.parentElement;
     cartItem.parentElement.removeChild(cartItem);
     // alert("Produto retirado do carrinho")
+}
+
+// let cartAdd = document.getElementsByClassName("add-car");
+
+// for (var i = 0; i < cartAdd.length; i++) {
+//     var button = cartAdd[i];
+//     button.addEventListener("click", add);
+// }
+// function add(event) {
+//     var button = event.target;
+//     var productsShop = button.parentElement;
+//     var titleElement = productsShop.getElementsByClassName("name-product")[0].innerText;
+//     var priceElement = productsShop.getElementsByClassName("valuer")[0].innerText;
+//     // var imgElement = productsShop.getElementsByClassName("img-products")[0];
+//     addToCart(titleElement, priceElement)
+//     // console.log(titleElement, priceElement);
+// }
+// function addToCart(titleElement, priceElement) {
+//     let shopItens = document.createElement("div")
+//     let cartIten =  document.getElementsByClassName("cart-content")[0]
+//     let cartItemName = cartIten.getElementsByClassName("cart-title-product")[0]
+//     for (var i = 0; i < cartItemName.length; i++) {
+//         alert("Produto adicionado no carrinho")
+//     }
+// }
+
+let cartAdd = document.getElementsByClassName("add-car");
+
+for (var i = 0; i < cartAdd.length; i++) {
+    var button = cartAdd[i];
+    button.addEventListener("click", add);
+}
+
+function add(event) {
+    var button = event.target;
+    var productsShop = button.parentElement;
+    var titleElement = productsShop.getElementsByClassName("name-product")[0].innerText;
+    var priceElement = productsShop.getElementsByClassName("valuer")[0].innerText;
+    addToCart(titleElement, priceElement);
+}
+
+function addToCart(title, price) {
+    let cartContent = document.querySelector(".cart-content");
+    let cartItemName = document.getElementsByClassName("cart-title-product");
+
+    if (cartItemName.length > 0) {
+        alert("Produto adicionado no carrinho");
+    }
 }
